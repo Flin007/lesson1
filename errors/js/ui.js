ce8481f0c36a28891d1f21e87c0e0ac3df82257a
Flin007 <Flin010@inbox.ru>
2018-05-24 11:05:51 +0300

    2е задание выполнено

--- errors/js/ui.js
+++ errors/js/ui.js
@@ -1,6 +1,5 @@
 'use strict';
-function handleClick {
-  openMail();
+const button = document.getElementById('open');
+button.onclick = function () {
+    openMail();
 }
-const button = window.getElementById('open');
-button.onclick = handleClick();
